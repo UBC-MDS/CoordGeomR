7f952ec1dd02c8bce48d28a04ce28c73a0f51b43
acheri5 <arlin.cherian@ubc.ca>
2022-01-25 13:26:33 -0800

    fixed an error in test file for pll lines function

--- tests/testthat/test-CoordGeomR.R
+++ tests/testthat/test-CoordGeomR.R
@@ -108,7 +108,7 @@ test_that("Non-numeric values for b2 should throw an error", {
 # testing distance calculations
 test_that("Error in distance calculation", {
   expect_equal(dist_pll_lines_2d(0, 1, 2), 1, tolerance = 1)
-  expect_equal(2.24, round(dist_pll_lines_2d(0, 1, 2), 2))
+  expect_equal(2.24, round(dist_pll_lines_2d(2, 4, -1), 2))
 })
 
 # testing distance calculation is just a numerical return value
